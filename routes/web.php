8cef999367ef2220ece83c6d1cb66f70a12680a9
Juanita <juanitaorukele10@gmail.com>
2024-12-11 13:46:11 +0000

    Updated web.php to iinlude Route::get to fix webpage not running

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecipeController;
-use App\Http\Controllers\CommentController;
+
 // Recipe routes
 Route::resource('recipes', RecipeController::class);
 
 // Comment routes
 Route::post('recipes/{recipe}/comments', [CommentController::class, 'store'])->name('comments.store');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Update the root route to render the index view for recipes
+Route::get('/', [RecipeController::class, 'index'])->name('home');
